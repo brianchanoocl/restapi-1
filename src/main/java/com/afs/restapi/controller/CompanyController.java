ac4d3251fd3905a5242852b65dc954d8ea905ce6
brianchanoocl <Delubdi118>
2021-12-08 18:36:39 +0800

    [restapi] [Brian] -feat implement functions in companyService class

--- src/main/java/com/afs/restapi/controller/CompanyController.java
+++ src/main/java/com/afs/restapi/controller/CompanyController.java
@@ -3,6 +3,7 @@ package com.afs.restapi.controller;
 import com.afs.restapi.entity.Company;
 import com.afs.restapi.entity.Employee;
 import com.afs.restapi.repository.CompanyRepository;
+import com.afs.restapi.service.CompanyService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,51 +12,51 @@ import java.util.List;
 @RestController
 @RequestMapping("companies")
 public class CompanyController {
-    private CompanyRepository companyRepository;
+    private CompanyService companyService;
 
-    public CompanyController(CompanyRepository companyRepository){
-        this.companyRepository = companyRepository;
+    public CompanyController(CompanyService companyService){
+        this.companyService = companyService;
     }
 
     @GetMapping
     public List<Company> getAllCompanies(){
-        return companyRepository.findAll();
+        return companyService.findAll();
     }
 
     @GetMapping("/{id}")
     public Company getCompanyById(@PathVariable Integer id) {
-        return companyRepository.findById(id);
+        return companyService.findById(id);
     }
 
     @GetMapping("/{id}/employees")
     public List<Employee> getCompanyEmployeeById(@PathVariable Integer id) {
-        return companyRepository.findById(id).getEmployees();
+        return companyService.findById(id).getEmployees();
     }
 
     @GetMapping(params = {"page", "pageSize"})
     public List<Company> getEmployeesByPage(@RequestParam Integer page, Integer pageSize) {
-        return companyRepository.findByPage(page, pageSize);
+        return companyService.findByPage(page, pageSize);
     }
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public Company createCompany(@RequestBody Company company){
-        return companyRepository.create(company);
+        return companyService.create(company);
     }
 
     @PutMapping("/{id}")
     public  Company editCompany(@PathVariable Integer id,@RequestBody Company updatedCompany){
-        Company company = companyRepository.findById(id);
+        Company company = companyService.findById(id);
         if(updatedCompany.getCompanyName() != null){
             company.setCompanyName(updatedCompany.getCompanyName());
         }
-        return companyRepository.update(id,company);
+        return companyService.update(id,company);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
     public void deleteCompany(@PathVariable Integer id){
-        Company company = companyRepository.findById(id);
-        companyRepository.delete(company);
+        Company company = companyService.findById(id);
+        companyService.delete(company);
     }
 }
--- src/main/java/com/afs/restapi/service/CompanyService.java
+++ src/main/java/com/afs/restapi/service/CompanyService.java
@@ -1,11 +1,38 @@
 package com.afs.restapi.service;
 
+import com.afs.restapi.entity.Company;
 import com.afs.restapi.repository.CompanyRepository;
+
+import java.util.List;
 
 public class CompanyService {
     private CompanyRepository companyRepository;
 
     public CompanyService(CompanyRepository companyRepository) {
         this.companyRepository = companyRepository;
+    }
+
+    public List<Company> findAll() {
+        return companyRepository.findAll();
+    }
+
+    public Company findById(Integer id) {
+        return companyRepository.findById(id);
+    }
+
+    public List<Company> findByPage(Integer page, Integer pageSize) {
+        return companyRepository.findByPage(page, pageSize);
+    }
+
+    public Company create(Company company) {
+        return companyRepository.create(company);
+    }
+
+    public Company update(Integer id, Company company) {
+        return companyRepository.update(id, company);
+    }
+
+    public void delete(Company company) {
+        companyRepository.delete(company);
     }
 }
