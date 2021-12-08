8444ff715013b7164adef011ae3b61747eed9591
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-08 15:10:40 +0800

    [RESTful][Koby] feat: should_return_all_employees_when_find_all_given_employees

--- src/main/java/com/afs/restapi/EmployeeService.java
+++ src/main/java/com/afs/restapi/EmployeeService.java
@@ -1,11 +1,19 @@
 package com.afs.restapi;
 
 import com.afs.restapi.objects.Employee;
+import com.afs.restapi.repository.EmployeeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
-
+@Service
 public class EmployeeService {
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
     public List<Employee> findAll() {
-        return  null;
+        return  employeeRepository.findAll();
     }
 }
