433e27478c5a6175995b7fabd789ab1357b67dab
kobywongchunkit <kobywongchunkit@gmail.com>
2021-12-08 15:07:42 +0800

    [RESTful][Koby] test: should_return_all_employees_when_find_all_given_employees

--- src/main/java/com/afs/restapi/EmployeeService.java
+++ src/main/java/com/afs/restapi/EmployeeService.java
@@ -1,4 +1,11 @@
 package com.afs.restapi;
 
+import com.afs.restapi.objects.Employee;
+
+import java.util.List;
+
 public class EmployeeService {
+    public List<Employee> findAll() {
+        return  null;
+    }
 }
--- src/test/java/com/afs/restapi/EmployeeServiceTest.java
+++ src/test/java/com/afs/restapi/EmployeeServiceTest.java
@@ -2,10 +2,19 @@ package com.afs.restapi;
 
 import com.afs.restapi.objects.Employee;
 import com.afs.restapi.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(SpringExtension.class)
 public class EmployeeServiceTest {
@@ -14,6 +23,18 @@ public class EmployeeServiceTest {
     @InjectMocks
     EmployeeService employeeService;
 
+    @Test
+    void should_return_all_employees_when_find_all_given_employees() {
+        //given
+        List<Employee> employees = Stream.of(new Employee(1,"Koby",3,"male",2))
+                .collect(Collectors.toList());
+        given(employeeRepository.findAll())
+                .willReturn(employees);
+        //when
+        List<Employee> actual =  employeeService.findAll();
+        //then
+        assertEquals(employees,actual);
+    }
 }
 
 
